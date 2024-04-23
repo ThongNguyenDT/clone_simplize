47c48033d4e271dacfbacd7bf278ef8eaa9fd34b
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2024-04-23 12:17:35 +0700

    start

---
+++ src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = ({className}: any) => {
+    return (
+        <div className={className}>
+            navbar
+        </div>
+    );
+};
+
+export default Navbar;
